6d67dae1aab238497f92f073f1ae1e5ea01480d3
allensll <sunll90@live.com>
2018-03-05 08:58:02 +0800

    update

---
+++ microDepminer/microDepminer_helper/src/main/java/com/allensll/algorithms/microDepminer/microDepminer_helper/modules/container/StorageSet.java
@@ -0,0 +1,12 @@
+package com.allensll.algorithms.microDepminer.microDepminer_helper.modules.container;
+
+public abstract class StorageSet {
+
+    @Override
+    public String toString() {
+
+        return this.toString_();
+    }
+
+    protected abstract String toString_();
+}
